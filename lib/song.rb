1657f39b4f8692a092dcb05f16c5a6fee21d451f
Seth Wax <sethlwax@gmail.com>
2018-04-03 19:48:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class Song
 
   @@count = 0
   @@artists = []
-  
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @@artists << artist
     @genre = genre
+    @@genres << genre
     @@count += 1
   end
 
